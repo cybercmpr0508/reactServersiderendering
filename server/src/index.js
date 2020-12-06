a4a7bc7e09e6dd4a9fae80d8c2d8cae4735628ce
CarlosmP <c.polanco@consuladord-ny.org>
2020-12-06 16:18:23 -0500

    "complete nav support fully-working"

--- server/src/index.js
+++ server/src/index.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
 
-  res.send(renderer());
+  res.send(renderer(req));
 });
 app.listen(3000, () => {
   console.log("Listening on port 3000");
